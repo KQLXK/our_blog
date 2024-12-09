495db07ef507bf1916651728107f246fbad3b066
KQLXK <yuchao1726199979@163.com>
2024-12-09 20:55:30 +0800

    完成删除文章接口

---
+++ service/article/article_delete.go
@@ -0,0 +1,58 @@
+package article
+
+import (
+	"errors"
+	"gorm.io/gorm"
+	"our_blog/model/dao"
+)
+
+type ArticleDeleteFlow struct {
+	ArticleId int64
+	UserId    int64
+}
+
+var (
+	DeleteUnauthorizedErr = errors.New("未被授权的删除")
+)
+
+func ArticleDelete(articleid int64, userid int64) error {
+	return NewArticleDeleteFlow(articleid, userid).Do()
+}
+
+func NewArticleDeleteFlow(articleid int64, userid int64) *ArticleDeleteFlow {
+	return &ArticleDeleteFlow{
+		ArticleId: articleid,
+		UserId:    userid,
+	}
+}
+
+func (f *ArticleDeleteFlow) Do() error {
+	if err := f.CheckData(); err != nil {
+		return err
+	}
+	if err := f.Delete(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (f *ArticleDeleteFlow) CheckData() error {
+	article, err := dao.NewArticleDaoInstance().GetArticleById(f.ArticleId)
+	if err == gorm.ErrRecordNotFound {
+		return ArticleNotFoundErr
+	} else if err != nil {
+		return err
+	}
+	if article.UserId != f.UserId {
+		return DeleteUnauthorizedErr
+	}
+	return nil
+}
+
+func (f *ArticleDeleteFlow) Delete() error {
+	err := dao.NewArticleDaoInstance().DeleteAnArticle(f.ArticleId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
